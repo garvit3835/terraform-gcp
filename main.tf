ce1c9f8170314a6ca1d885bc89128fe381bc757b
garvit1189 <97427673+garvit3835@users.noreply.github.com>
2024-07-13 13:01:48 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "google" {
+  project = "analog-foundry-418510"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  count = 2
+  name     = "my-infrasity-bucket-1189"
+  location = "US"
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.my_bucket.name
+}
