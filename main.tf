89281c901e0de05414a4e17f44942009fa365895
garvit1189 <97427673+garvit3835@users.noreply.github.com>
2024-07-16 14:20:38 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,8 +1,20 @@
 provider "google" {
-  # credentials = jsondecode(var.google_credentials)
-  project = "analog-foundry-418510"
-  region  = "us-central1"
+  project     = "analog-foundry-418510"
+  region      = "us-central1"
+  credentials = file("C:/Users/Garvit/Downloads/analog-foundry-418510-6c099625a9d3.json")
 }
+
+  # provider "google" {
+  #   project     = "your-gcp-project-id"
+  #   region      = "us-central1"
+  #   credentials = file("path/to/your/service-account-file.json")
+  # }
+
+  # resource "google_storage_bucket" "my_bucket" {
+  # name     = "your-bucket-name"
+  # location = "your-bucket-location"
+  # }
+
 
 resource "google_storage_bucket" "my_bucket" {
   name     = "my-infrasity-bucket-1189"
